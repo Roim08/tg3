c3e255304566070dbea6d0153500106560ba9250
Your Name <you@example.com>
2020-11-20 02:28:54 +0300

    second

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ async def on_startup(dp):
     from utils.notify_admins import on_startup_notify
     await on_startup_notify(dp)
     await set_default_commands(dp)
-
+asd
 
 if __name__ == '__main__':
     from aiogram import executor
